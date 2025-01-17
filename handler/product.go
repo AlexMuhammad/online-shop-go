a962b38020368f736362ef68aa2c1f656bb3eccd
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2025-01-17 23:33:36 +0700

    feat: show product by id

--- handler/product.go
+++ handler/product.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"database/sql"
+	"errors"
+	"log"
 	"online-shop-fastcampus/model"
 
 	"github.com/gin-gonic/gin"
@@ -11,10 +13,31 @@ func ListProducts(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		products, err := model.SelectProduct(db)
 		if err != nil {
+			log.Printf("Something went wrong %v\n", err)
 			c.JSON(500, gin.H{"error": err})
 			return
 		}
 
 		c.JSON(200, products)
+	}
+}
+
+func GetProduct(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+
+		product, err := model.SelectProductByID(db, id)
+		log.Printf("value test %v, test err %v\n", product, err)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				c.JSON(404, gin.H{"error": "Product not found"})
+				return
+			}
+			log.Printf("Something went wrong %v\n", err)
+			c.JSON(500, gin.H{"error": err})
+			return
+		}
+
+		c.JSON(200, product)
 	}
 }
