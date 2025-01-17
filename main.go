ca78fb9e9eeff0474dcc83fed14934bd8e25239b
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2025-01-17 20:31:28 +0700

    feat: folder structure

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
-	fmt.Println("Hello Online Shop")
+	db, err := sql.Open("pgx", "postgres://postgres:password@localhost:5432/onlineshopfastcampus?sslmode=disable")
+	if err != nil {
+		fmt.Printf("Failed to connect db %v\n", err)
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		fmt.Printf("Failed to verify db connection %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Success to connect db \n")
+
 }
